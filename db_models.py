71cd4af4c0b86414b6eda214eda2ecd79c58671e
Neto test <kordik.a@yndex.ru>
2026-01-08 15:57:17 +0300

    [59367780] - added missing data

--- db_models.py
+++ db_models.py
@@ -1,50 +1,67 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 
-
 Base = declarative_base()
 
-#Model BD
-class Candidates(Base):
-    __tablename__ = 'candidates'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+class Candidate(Base):
+    __tablename__ = "candidates"
+
+    candidate_id = sq.Column(sq.Integer, primary_key=True)
     vk_id = sq.Column(sq.Integer, unique=True, nullable=False)
     first_name = sq.Column(sq.String, nullable=False)
     last_name = sq.Column(sq.String, nullable=False)
-    age = sq.Column(sq.Integer, unique=True, nullable=False)
-    gender = sq.Column(sq.String, nullable= False)
+    age = sq.Column(sq.Integer, nullable=False)
+    gender = sq.Column(sq.String, nullable=False)
     city = sq.Column(sq.String, nullable=False)
     profile_link = sq.Column(sq.String, nullable=False)
 
-    favorites = relationship('user', secondary='users_to_favorites', backref='candidates')
-    blacklist = relationship('user', secondary='users_to_blacklist', backref='candidates')
-
+    favorites = relationship(
+        "User", secondary="users_to_favorites", backref="candidates"
+    )
+    blacklist = relationship(
+        "User", secondary="users_to_blacklist", backref="candidates"
+    )
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = "users"
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.Integer, primary_key=True)
     vk_id = sq.Column(sq.Integer, unique=True, nullable=False)
-    age = sq.Column(sq.Integer, unique=True, nullable=False)
-    gender = sq.Column(sq.String, nullable= False)
+    age = sq.Column(sq.Integer, nullable=False)
+    gender = sq.Column(sq.String, nullable=False)
     city = sq.Column(sq.String, nullable=False)
 
 
-
 class Photo(Base):
-    __tablename__ = 'photo'
-    id = sq.Column(sq.Integer, primary_key=True)
-    vk_id = sq.Column(sq.Integer, unique=True, nullable=False)
-    candidates_id = sq.Column(sq.Integer, sq.ForeignKey('candidates.id'), nullable=False)
-    likes_count = sq.Column(sq.Integer, unique=True, nullable=False)
+    __tablename__ = "photos"
+
+    photo_id = sq.Column(sq.Integer, primary_key=True)
+    vk_photo_id = sq.Column(sq.Integer, unique=True, nullable=False)
+    candidates_id = sq.Column(
+        sq.Integer, sq.ForeignKey("candidates.candidate_id"), nullable=False
+    )
+    likes_count = sq.Column(sq.Integer, nullable=False)
     photo_link = sq.Column(sq.String, nullable=False)
 
-    candidates = relationship('candidates', backref='photo')
+    candidates = relationship("Candidates", backref="photo")
 
 
-def create_tables(engine):
-    # Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+users_to_favorites = sq.Table(
+    "users_to_favorites",
+    Base.metadata,
+    sq.Column(
+        "candidates.candidate_id", sq.Integer, sq.ForeignKey("candidates.candidate_id")
+    ),
+    sq.Column("users.user_id", sq.Integer, sq.ForeignKey("users.user_id")),
+)
 
+users_to_blacklist = sq.Table(
+    "users_to_blacklist",
+    Base.metadata,
+    sq.Column(
+        "candidates.candidate_id", sq.Integer, sq.ForeignKey("candidates.candidate_id")
+    ),
+    sq.Column("users.user_id", sq.Integer, sq.ForeignKey("users.user_id")),
+)
